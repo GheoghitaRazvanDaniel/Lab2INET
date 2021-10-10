07fdc9e54e3d490675fb1434df1e03d4d1813e98
Razvan-Daniel Gheorghita <razvan.gheorghita@info.uaic.ro>
2021-10-10 19:47:33 +0300

    Kata Part I

---
+++ Lab2Part1/Lab2Part1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab2Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file_path = @"G:\INET\Lab2\Lab2Part1\Lab2Part1\Lab2Part1\weather.dat";
+            List<string> file_lines = GetFileContent(file_path);
+            // Linia cu numele coloanelor
+            file_lines.RemoveAt(0);
+            // Linia goala dintre numele coloanelor si valorile lor
+            file_lines.RemoveAt(0);
+            file_lines.RemoveAt(file_lines.Count - 1);
+            List<WeatherData> weatherData = new List<WeatherData>();
+            var i = 0;
+            foreach (string line in file_lines)
+            {
+                
+                string[] data = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                WeatherData day_data = new WeatherData(data);
+                weatherData.Add(day_data);
+            }
+            Console.WriteLine("The day with the lowest temperature spread is " + GetSmallestTemperatureSpreadDay(weatherData));
+        }
+
+        static List<string> GetFileContent(string file_path)
+        {
+            List<string> lines = new List<string>();
+            foreach(string line in System.IO.File.ReadLines(file_path))
+            {
+                lines.Add(line);
+            }
+            return lines;
+        }
+
+        static decimal GetSmallestTemperatureSpreadDay(List<WeatherData> weatherDatas)
+        {
+            decimal day = weatherDatas[0].GetDayNumber();
+            decimal minimalSpread = weatherDatas[0].GetTemperatureSpread();
+            foreach(WeatherData day_data in weatherDatas)
+            {
+                if(minimalSpread>day_data.GetTemperatureSpread())
+                {
+                    day = day_data.GetDayNumber();
+                    minimalSpread = day_data.GetTemperatureSpread();
+                }
+            }
+            return day;
+        }
+    }
+}
---
+++ Lab2Part1/Lab2Part1/WeatherData.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Lab2Part1
+{
+    public class WeatherData
+    {
+        public decimal Day;
+        public decimal MaxTemperature;
+        public decimal MinTemperature;
+        public decimal AverageTemperature;
+        public decimal HDDay;
+        public double AverageDP;
+        public string OneHrP;
+        public double TPcpn;
+        public string WxType;
+        public decimal PDir;
+        public double AverageSp;
+        public decimal Dir;
+        public decimal MaxS;
+        public double SkyC;
+        public decimal MaxR;
+        public decimal MinR;
+        public double AverageSLP;
+
+        public WeatherData(string[] data)
+        {
+            this.Day = Convert.ToDecimal(data[0]);
+            this.MaxTemperature = Convert.ToDecimal(data[1]);
+            this.MinTemperature = Convert.ToDecimal(data[2]);
+            this.AverageTemperature = Convert.ToDecimal(data[3]);
+            this.HDDay = Convert.ToDecimal(data[4]);
+            this.AverageDP = Convert.ToDouble(data[5]);
+            if(data[6] == "0")
+            {
+                this.OneHrP = "No data";
+            }
+            else
+            {
+                this.OneHrP = data[6];
+            }
+            this.TPcpn = Convert.ToDouble(data[7]);
+            if(data[8] == "0")
+            {
+                this.WxType = "No data";
+            }
+            else
+            {
+                this.WxType = data[8];
+            }
+            this.PDir = Convert.ToDecimal(data[9]);
+            this.AverageSp = Convert.ToDouble(data[10]);
+            this.Dir = Convert.ToDecimal(data[11]);
+            this.MaxS = Convert.ToDecimal(data[12]);
+            this.SkyC = Convert.ToDouble(data[13]);
+            this.MaxR = Convert.ToDecimal(data[14]);
+            this.MinR = Convert.ToDecimal(data[15]);
+            this.AverageSLP = Convert.ToDouble(data[16]);
+        }
+
+        public decimal GetTemperatureSpread()
+        {
+            return this.MaxTemperature - this.MinTemperature;
+        }
+
+        public decimal GetDayNumber()
+        {
+            return this.Day;
+        }
+    }
+}
